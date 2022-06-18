dfe25c0b64a8d832746640527a8c88468d83d388
Dimasidn <diagra99@gmail.com>
2022-06-18 20:27:01 +0700

    membuat produk_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:toko_furniture_online/screen/produk_screen.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  // Untuk scroll web agar bisa berfungsi secara horizontal
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
 }
 
 class MyApp extends StatelessWidget {
@@ -9,15 +20,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scrollBehavior: MyCustomScrollBehavior(),
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(child: Text('Flutter')),
-      ), //Ketik nama class dari screen lain di sini [ home: namaclass(), ]
+      home:
+          const ProdukScreen(), //Ketik nama class dari screen lain di sini [ home: namaclass(), ]
     );
   }
 }
