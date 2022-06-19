b7e602763cee6ba368f70bf1b6f746f664e48168
Dimasidn <diagra99@gmail.com>
2022-06-19 13:17:52 +0700

    creating keranjang_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:toko_furniture_online/screen/keranjang_screen.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyCustomScrollBehavior extends MaterialScrollBehavior {
-  // Untuk scroll web agar bisa berfungsi secara horizontal
-  @override
-  Set<PointerDeviceKind> get dragDevices => {
-        PointerDeviceKind.touch,
-        PointerDeviceKind.mouse,
-      };
 }
 
 class MyApp extends StatelessWidget {
@@ -20,15 +12,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      scrollBehavior: MyCustomScrollBehavior(),
+      scrollBehavior: MaterialScrollBehavior().copyWith(
+          overscroll: false,
+          dragDevices: {PointerDeviceKind.touch, PointerDeviceKind.mouse}),
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(child: Text('Flutter')),
-      ), //Ketik nama class dari screen lain di sini [ home: namaclass(), ]
+      home:
+          const KeranjangScreen(), //Ketik nama class dari screen lain di sini [ home: namaclass(), ]
     );
   }
 }
